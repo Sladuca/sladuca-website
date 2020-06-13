6ab97ae9f03d7a6b9e699565a2584feaf2e858f5
Sebastien La Duca <sladuca777@gmail.com>
2020-06-13 19:08:43 -0400

    configure tailwind with antd colors

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="p-4 bg-white rounded shadow">
+      <h1 className="leading-normal text-purple-500">Next.js</h1>
+      <p className="text-gray-500">with Tailwind CSS</p>
+    </div>
+  );
+}
