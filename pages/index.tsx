05efe9657215d80131d56199ca61db7f399c732f
Sebastien La Duca <sladuca777@gmail.com>
2020-06-14 09:26:53 -0400

    give up on workers site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="p-4 bg-white rounded shadow">
-      <h1 className="leading-normal text-purple-500">Next.js</h1>
-      <p className="text-gray-500">with Tailwind CSS</p>
+    <div className="grid grid-cols-1 gap-4">
+      <div className="flex justify-center py-8">
+        <h1>Sebastien La Duca</h1>
+      </div>
+      <div className="flex justify-center">
+        <img src="/monogram.svg" className="h-32"/>
+      </div>
+      <div className="flex justify-center">
+        <div className="max-w-sm overflow-hidden">
+          <div className="grid grid-cols-1 gap-2 px-6 py-4">
+            <p>
+              Hi! My name is Sebastien.
+            </p>
+            <p>
+              I'm a computer engineering student, blockchain evangelist, armchair philosopher, and budding entrepreneur on a mission to build a future where everyone can make a living by being themseleves.
+            </p>
+            <p>
+              I'm using this website primarily as a place for me to share what I'm doing, what I'm learning, and what I'm thinking about with anyone who wishes to read.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 module.exports = {
   purge: [],
+  // purge: {
+  //   enabled: true,
+  //   content: ['./src/**/*.html'],
+  // },
   theme: {
     // These are the ant design colors: https://ant.design/docs/spec/colors
     colors: {
+      white: '#ffffff',
+      black: '#000000',
+      gray: {
+        '1': '#ffffff',
+        '2': '#fafafa',
+        '3': '#f5f5f5',
+        '4': '#f0f0f0',
+        '5': '#d9d9d9',
+        '6': '#bfbfbf',
+        '7': '#8c8c8c',
+        '8': '#595959',
+        '9': '#434343',
+        '10': '#262626',
+        '11': '#1f1f1f',
+        '12': '#141414',
+        '12': '#000000'
+      },
       red: {
         '1': '#fff1f0',
         '2': '#ffccc7',
