4d3b1e3ff962bb8eabece890c737f8e4b9b29f29
Sebastien La Duca <sladuca777@gmail.com>
2020-06-17 00:11:02 -0400

    monogram tweaks

--- components/wrapper.tsx
+++ components/wrapper.tsx
@@ -6,8 +6,8 @@ export default function Wrapper({ children }) {
   return (
     <div id="outer-container">
       <Menu disableAutoFocus pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
-        <div><Link href="/"><a id="home" className="menu-item">Home</a></Link></div>
-        <div><Link href="/learning_layer_one"><a id="llo" className="menu-item">Learning Layer One</a></Link></div>
+        <div><Link href="/"><a id="home" className="menu-item text-gray-3">Home</a></Link></div>
+        <div><Link href="/learning_layer_one"><a id="llo" className="menu-item text-gray-3">Learning Layer One</a></Link></div>
       </Menu>
       <main id="page-wrap">
         { children }
